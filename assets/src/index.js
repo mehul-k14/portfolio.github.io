6a681d8485914032284dfb38cf00bd8b6037f841
Mehul <placedownmehul@gmail.com>
2023-01-02 12:34:52 +0530

    "remove service worker"

--- assets/src/index.js
+++
@@ -1,7 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js').then(registeration => {
-        console.log("SW Resgisterd");
-    }).catch(error => {
-        console.log("SW registration Failed");
-    });
-}
